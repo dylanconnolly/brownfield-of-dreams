350c3fc4d6b61edaccc33fc4a44cb6204013a08d
Scott Regenthal <scottregenthal@gmail.com>
2020-02-04 14:29:40 -0700

    Add blank line after modifier; rename variable; reformat conditional

--- app/services/tutorial_sequencer.rb
+++ app/services/tutorial_sequencer.rb
@@ -9,6 +9,7 @@ class TutorialSequencer
   end
 
   private
+
   attr_reader :tutorial, :sequenced_video_ids
 
   def videos
@@ -17,13 +18,10 @@ class TutorialSequencer
 
   def update_position_if_changed!
     sequenced_video_ids.each.with_index(1) do |video_id, index|
-      video = videos.find do |video|
+      initial_video = videos.find do |video|
         video.id == video_id.to_i
       end
-
-      if video.position != index
-        video.update(position: index)
-      end
+      initial_video.update(position: index) if initial_video.position != index
     end
   end
 end
