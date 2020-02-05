cb8e89d8addf5cbad60b2eb05e7c246d7ee896b1
Dylan Connolly <dylanconnolly145@gmail.com>
2020-02-05 12:36:09 -0700

    Add test for no_classroom class method

--- spec/models/tutorial_spec.rb
+++ spec/models/tutorial_spec.rb
@@ -1,4 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Tutorial, type: :model do
+  describe "class methods" do
+    it '.no_classroom' do
+
+      create_list(:tutorial, 3,title: 'This Video is for Logged In Users Only', classroom: true)
+      non_classroom_tutorials = create_list(:tutorial, 3, title: 'This tutorial can be viewed by Visitors')
+
+      expect(Tutorial.no_classroom.count).to eq(3)
+
+      Tutorial.no_classroom.each do |tutorial|
+        expect(tutorial.classroom).to eq(false)
+      end
+    end
+  end
+
 end
