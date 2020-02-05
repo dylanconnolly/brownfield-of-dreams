62caab1ac49be0672c3d08d4cf41a7fd72726f5c
Dylan Connolly <dylanconnolly145@gmail.com>
2020-02-05 12:29:04 -0700

    Add conditional to utilize no_classroom method on tutorial if current_user is nil (visitor)

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,22 @@
 class WelcomeController < ApplicationController
   def index
-    @tutorials = if params[:tag]
-                   Tutorial
-                     .tagged_with(params[:tag])
-                     .paginate(page: params[:page], per_page: 5)
-                 else
-                   Tutorial.all.paginate(page: params[:page], per_page: 5)
-                 end
+    if current_user
+      @tutorials = if params[:tag]
+                      Tutorial
+                      .tagged_with(params[:tag])
+                      .paginate(page: params[:page], per_page: 5)
+                    else
+                      Tutorial.all.paginate(page: params[:page], per_page: 5)
+                    end
+    else
+      @tutorials = if params[:tag]
+                      Tutorial
+                      .no_classroom
+                      .tagged_with(params[:tag])
+                      .paginate(page: params[:page], per_page: 5)
+                    else
+                      Tutorial.no_classroom.paginate(page: params[:page], per_page: 5)
+                    end
+    end
   end
 end
