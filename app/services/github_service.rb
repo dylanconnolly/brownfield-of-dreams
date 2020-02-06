1523dbd4063ab15be7e3e83ebca3c729d598221d
Dylan Connolly <dylanconnolly145@gmail.com>
2020-02-06 15:19:36 -0700

    Create github service to handle api requests to github
    Co-Authored-By: Scott Regenthal <freeheeling@users.noreply.github.com>

---
+++ app/services/github_service.rb
@@ -0,0 +1,22 @@
+class GithubService
+
+  def get_repos
+    get_json('/user/repos')
+  end
+
+
+
+  private
+
+  def connection
+    Faraday.new('https://api.github.com') do |f|
+      f.headers["Authorization"] = "token #{ENV["GITHUB_PERSONAL_TOKEN"]}"
+      f.adapter Faraday.default_adapter
+    end
+  end
+
+  def get_json(uri)
+    response = connection.get(uri)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
