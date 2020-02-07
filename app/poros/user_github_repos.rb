45ebfd07128bf13f5cf48de553cf56b038054b6b
Scott Regenthal <50811220+freeheeling@users.noreply.github.com>
2020-02-06 19:19:22 -0700

    Update user_github_repos.rb

--- app/poros/user_github_repos.rb
+++ app/poros/user_github_repos.rb
@@ -1,10 +1,7 @@
 class UserGithubRepos
-
   def repos
     return @repo_data if @repo_data
-
     service = GithubService.new
-
     @repo_data = service.get_repos.map do |repo_data|
       Repo.new(repo_data)
     end
