c346fb5464c4b8f171108d203d47d45427bd3964
Scott Regenthal <scottregenthal@gmail.com>
2020-02-07 07:39:17 -0700

    Refactor single index method into two private methods upon which to call

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,22 +1,36 @@
 class WelcomeController < ApplicationController
   def index
     if current_user
-      @tutorials = if params[:tag]
-                      Tutorial
-                      .tagged_with(params[:tag])
-                      .paginate(page: params[:page], per_page: 5)
-                    else
-                      Tutorial.all.paginate(page: params[:page], per_page: 5)
-                    end
+      classroom_tutorial
     else
-      @tutorials = if params[:tag]
-                      Tutorial
-                      .no_classroom
-                      .tagged_with(params[:tag])
-                      .paginate(page: params[:page], per_page: 5)
-                    else
-                      Tutorial.no_classroom.paginate(page: params[:page], per_page: 5)
-                    end
+      no_classroom_tutorial
     end
+  end
+
+  private
+
+  def classroom_tutorial
+    @tutorials = if params[:tag]
+                   Tutorial
+                     .tagged_with(params[:tag])
+                     .paginate(page: params[:page], per_page: 5)
+                 else
+                   Tutorial
+                     .all
+                     .paginate(page: params[:page], per_page: 5)
+                 end
+  end
+
+  def no_classroom_tutorial
+    @tutorials = if params[:tag]
+                   Tutorial
+                     .no_classroom
+                     .tagged_with(params[:tag])
+                     .paginate(page: params[:page], per_page: 5)
+                 else
+                   Tutorial
+                     .no_classroom
+                     .paginate(page: params[:page], per_page: 5)
+                 end
   end
 end
