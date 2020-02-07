7d50565147e39ab3273bdde22dcecb5b531ba0b8
Scott Regenthal <scottregenthal@gmail.com>
2020-02-07 16:08:29 -0700

    Create video model spec for relationships and attribute validations

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,5 +1,10 @@
 class Video < ApplicationRecord
+  belongs_to :tutorial
   has_many :user_videos
   has_many :users, through: :user_videos
-  belongs_to :tutorial
+
+  validates_presence_of :title
+  validates_presence_of :description
+  validates_presence_of :thumbnail
+  validates_presence_of :position
 end
--- spec/factories/videos.rb
+++ spec/factories/videos.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :video do
     title { Faker::Pokemon.name }
     description { Faker::SiliconValley.motto }
+    thumbnail { Faker::Placeholdit.image }
     video_id { Faker::Crypto.md5 }
     tutorial
   end
