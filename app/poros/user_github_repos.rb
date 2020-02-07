cb62b261a57f79badcc9e8124da494ca49264534
Scott Regenthal <scottregenthal@gmail.com>
2020-02-07 07:30:11 -0700

    Memoize repos

--- app/poros/user_github_repos.rb
+++ app/poros/user_github_repos.rb
@@ -1,8 +1,6 @@
 class UserGithubRepos
   def repos
-    return @repo_data if @repo_data
-    service = GithubService.new
-    @repo_data = service.get_repos.map do |repo_data|
+    @repos ||= GithubService.new.user_repos.map do |repo_data|
       Repo.new(repo_data)
     end
   end
