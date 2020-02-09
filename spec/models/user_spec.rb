5549020cb744b5e1e246d0807ddc6217f9ffa2f5
Scott Regenthal <scottregenthal@gmail.com>
2020-02-08 19:31:27 -0700

    Merge branch 'master' of https://github.com/freeheeling/brownfield-of-dreams

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,8 +8,8 @@ RSpec.describe User, type: :model do
 
   describe 'validations' do
     it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
     it { should validate_presence_of(:first_name) }
-    it { should validate_presence_of(:password) }
   end
 
   describe 'roles' do
