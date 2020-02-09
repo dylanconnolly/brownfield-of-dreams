faba1fd86b221c264b0a6e5ba72eeff527234bbd
Scott Regenthal <scottregenthal@gmail.com>
2020-02-08 20:42:15 -0700

    Create test using omniauth mock auth to test github oauth process

---
+++ spec/features/user/user_can_connect_to_github_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe 'A registered User' do
+  it 'can connect to GitHub', :vcr do
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
+      provider: 'github',
+      uid: '12345',
+      credentials: {
+        token: ENV['GITHUB_PERSONAL_TOKEN'],
+        expires: false
+      },
+    })
+
+    user = create(:user)
+
+    stub_user(user)
+
+    visit dashboard_path
+
+    click_button 'Connect to GitHub'
+
+    expect(user.github_token).to eq(ENV['GITHUB_PERSONAL_TOKEN'])
+    expect(current_path).to eq(dashboard_path)
+  end
+end
