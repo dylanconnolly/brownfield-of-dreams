deedcb023abe4d0a98d2b222dd7bc9b53d8a19d2
Dylan Connolly <dylanconnolly145@gmail.com>
2020-02-11 16:34:35 -0700

    Update model tests to test for 'github_handle' attribute instead of 'login'
    Co-Authored-By: Scott Regenthal <freeheeling@users.noreply.github.com>

--- spec/models/follower_spec.rb
+++ spec/models/follower_spec.rb
@@ -3,16 +3,16 @@ require 'rails_helper'
 RSpec.describe Follower, type: :model do
   it 'exists', :vcr do
     attributes = {
-      login: 'mikecm1141',
+      github_handle: 'mikecm1141',
       html_url: 'https://github.com/mikecm1141'
     }
 
     follower = Following.new(attributes)
-    expected_login = follower.login
+    expected_login = follower.github_handle
     expected_html_url = follower.html_url
 
     expect(follower).to be_a Following
-    expect(follower.login).to be(expected_login)
+    expect(follower.github_handle).to be(expected_login)
     expect(follower.html_url).to be(expected_html_url)
   end
 end
--- spec/models/following_spec.rb
+++ spec/models/following_spec.rb
@@ -3,16 +3,16 @@ require 'rails_helper'
 RSpec.describe Following, type: :model do
   it 'exists', :vcr do
     attributes = {
-      login: 'BabsLabs',
+      github_handle: 'BabsLabs',
       html_url: 'https://github.com/BabsLabs'
     }
 
     following = Following.new(attributes)
-    expected_login = following.login
+    expected_login = following.github_handle
     expected_html_url = following.html_url
 
     expect(following).to be_a Following
-    expect(following.login).to be(expected_login)
+    expect(following.github_handle).to be(expected_login)
     expect(following.html_url).to be(expected_html_url)
   end
 end
