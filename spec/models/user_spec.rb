7c1c2c3132feda32311cd34159e9cc880e7c7e81
Scott Regenthal <scottregenthal@gmail.com>
2020-02-11 14:49:13 -0700

    Add new relationships to user and remove validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe User, type: :model do
   describe 'relationships' do
     it { should have_many(:user_videos) }
     it { should have_many(:videos).through(:user_videos) }
+    it { should have_many(:friendships) }
+    it { should have_many(:friends).through(:friendships) }
   end
 
   describe 'validations' do
