50db44ebc42432c0cc955a9d352717646bed096a
Scott Regenthal <scottregenthal@gmail.com>
2020-02-11 14:48:25 -0700

    Add handle to user table, create friendships table, migrate DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_10_201338) do
+ActiveRecord::Schema.define(version: 2020_02_11_183516) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "taggings", id: :serial, force: :cascade do |t|
     t.integer "tag_id"
@@ -69,6 +76,7 @@ ActiveRecord::Schema.define(version: 2020_02_10_201338) do
     t.datetime "updated_at", null: false
     t.string "github_token"
     t.boolean "activated", default: false
+    t.string "github_handle"
     t.index ["email"], name: "index_users_on_email"
   end
 
