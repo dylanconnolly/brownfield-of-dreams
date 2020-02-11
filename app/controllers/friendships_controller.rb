9e51774da7166aeadd7b4259a2d1e8355d20261b
Dylan Connolly <dylanconnolly145@gmail.com>
2020-02-11 15:58:24 -0700

    Add friendship create functionality which has happy and sad path.
    Co-Authored-By: Scott Regenthal <freeheeling@users.noreply.github.com>

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,5 +1,13 @@
 class FriendshipsController < ApplicationController
 
   def create
+    friend = User.find_by(github_handle: params[:handle])
+    friendship = current_user.friendships.new(friend_id: friend.id)
+    if friendship.save
+      flash[:success] = "Added friend!"
+    else
+      flash[:error] = "Unable to add friend. That user does not have a registered account with Turing Tutorials."
+    end
+    redirect_to dashboard_path
   end
 end
