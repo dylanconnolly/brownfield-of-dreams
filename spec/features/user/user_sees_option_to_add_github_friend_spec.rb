161a82b4c2420c1b7936080a8dc4b4b9b416060c
Dylan Connolly <dylanconnolly145@gmail.com>
2020-02-11 15:26:30 -0700

    Add assertions for certain followers or followings to have an Add Friend button based on whether that github handle matches with a github handle in the Users table

--- spec/features/user/user_sees_option_to_add_github_friend_spec.rb
+++ spec/features/user/user_sees_option_to_add_github_friend_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 describe 'as a registered User' do
   it 'sees option to add github follower/following as friend' do
     user = create(:user, github_token: ENV['GITHUB_PERSONAL_TOKEN'])
+    follower = create(:user, github_handle: "follower_handle_1")
+    following = create(:user, github_handle: "following_handle_1")
 
     stub_user(user)
 
@@ -20,8 +22,34 @@ describe 'as a registered User' do
 
     visit dashboard_path
 
+    within('#github-followers') do
+      expect(page).to have_content("follower_handle_1")
+      expect(page).to have_content("follower_handle_2")
+    end
 
-      require "pry"; binding.pry
+    within(first('.github-follower')) do
+      expect(page).to have_content("follower_handle_1")
+      expect(page).to have_button("Add as Friend")
+    end
 
+    within(all('.github-follower')[1]) do
+      expect(page).to have_content("follower_handle_2")
+      expect(page).to_not have_button("Add as Friend")
+    end
+
+    within('#github-followings') do
+      expect(page).to have_content("following_handle_1")
+      expect(page).to have_content("following_handle_2")
+    end
+
+    within(first('.github-following')) do
+      expect(page).to have_content("following_handle_1")
+      expect(page).to have_button("Add as Friend")
+    end
+
+    within(all('.github-following')[1]) do
+      expect(page).to have_content("following_handle_2")
+      expect(page).to_not have_button("Add as Friend")
+    end
   end
 end
