e2d03bd88c829df9dbf3e785f03bb9ff7f677665
Dylan Connolly <dylanconnolly145@gmail.com>
2020-02-11 16:49:32 -0700

    Add private method for frienship_params to be strong params
    Co-Authored-By: Scott Regenthal <freeheeling@users.noreply.github.com>

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,7 +1,7 @@
 class FriendshipsController < ApplicationController
 
   def create
-    friend = User.find_by(github_handle: params[:handle])
+    friend = User.find_by(github_handle: friendship_params[:handle])
     friendship = current_user.friendships.new(friend_id: friend.id)
     if friendship.save
       flash[:success] = "Added friend!"
@@ -9,5 +9,11 @@ class FriendshipsController < ApplicationController
       flash[:error] = "Unable to add friend. That user does not have a registered account with Turing Tutorials."
     end
     redirect_to dashboard_path
+  end
+
+  private
+
+  def friendship_params
+    params.permit(:user_id, :handle)
   end
 end
